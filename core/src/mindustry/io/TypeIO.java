TinyLake/MindustryX-BE

S: update TypeIO.writeRequest

core/src/mindustry/io/TypeIO.java
@@ -174,7 +174,7 @@ public class TypeIO{
 
     //this is irrelevant.
     static final WeaponMount[] noMounts = {};
-    
+
     public static WeaponMount[] readMounts(Reads read){
         read.skip(read.b() * (1 + 4 + 4));
 
@@ -248,7 +248,7 @@ public class TypeIO{
             write.s(request.block.id);
             write.b((byte)request.rotation);
             write.b(1); //always has config
-            writeObject(write, request.config);
+            writeObject(write, net.server() ? null : request.config);
         }
     }
 

TinyLake/MindustryX

(cherry picked from commit cea222ab0573303337aa91f5ffe5447351f7c10a)
(cherry picked from commit 8ba4a6e1e5dd75a779d3509ebcde1c6dff6bd9d0)

core/src/mindustry/io/TypeIO.java
@@ -381,8 +381,8 @@ public class TypeIO{
         if(!plan.breaking){
             write.s(plan.block.id);
             write.b((byte)plan.rotation);
-            write.b(1); //always has config
-            writeObject(write, plan.config);
+            write.b(0); //always has config
+            writeObject(write, net.server() ? null : plan.config);
         }
     }
 