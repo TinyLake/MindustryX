9d70faf5983cda8d4c9fcf07e559f08531750af3
way-zer <himc.wicp@gmail.com>
2024-11-16 23:02:04 +0800

    Revert "复用 detach-camera，删除 viewMode"
    This reverts commit d14577bc

--- src/mindustryX/features/ui/auxiliary/ScriptButtons.java
+++ src/mindustryX/features/ui/auxiliary/ScriptButtons.java
@@ -27,17 +27,27 @@ public class ScriptButtons extends AuxiliaryTools.Table{
             }
         }).tooltip("在建造列表加入被摧毁建筑");
         button(Items.copper.emoji(), RStyles.clearLineNoneTogglet, () -> AutoFill.enable ^= true).tooltip("一键装弹").checked((b) -> AutoFill.enable);
-        addSettingButton(Icon.modeAttack, "autotarget", "自动攻击");
-        addSettingButton(new TextureRegionDrawable(UnitTypes.vela.uiIcon), "forceBoost", "强制助推");
-        addSettingButton(Icon.eyeSmall, "detach-camera", "视角脱离玩家");
+        addSettingButton(Icon.modeAttack, "autotarget", "自动攻击", null);
+        addSettingButton(new TextureRegionDrawable(UnitTypes.vela.uiIcon), "forceBoost", "强制助推", null);
+        addSettingButton(Icon.eyeSmall, "viewMode", "视角脱离玩家", s -> {
+            if(s){
+                if(control.input instanceof DesktopInput desktopInput){
+                    desktopInput.panning = true;
+                }
+            }else{
+                Core.camera.position.set(player);
+            }
+        });
     }
 
-    protected void addSettingButton(Drawable icon, String settingName, String description){
+    protected void addSettingButton(Drawable icon, String settingName, String description, Boolc onClick){
         button(icon, RStyles.clearLineNoneTogglei, iconMed, () -> {
             boolean setting = Core.settings.getBool(settingName);
 
             Core.settings.put(settingName, !setting);
             UIExt.announce("已" + (setting ? "取消" : "开启") + description);
+
+            if(onClick != null) onClick.get(!setting);
         }).tooltip(description, true).checked(b -> Core.settings.getBool(settingName));
     }
 }
