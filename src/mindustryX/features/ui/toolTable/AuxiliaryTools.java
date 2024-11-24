4a0ab7034ed77880cba7b3cde9d2302a0d590ed2
way-zer <himc.wicp@gmail.com>
2024-11-24 13:53:26 +0800

    移动辅助器位置，整理工具栏

---
+++ src/mindustryX/features/ui/toolTable/AuxiliaryTools.java
@@ -0,0 +1,166 @@
+package mindustryX.features.ui.toolTable;
+
+import arc.*;
+import arc.graphics.g2d.*;
+import arc.scene.style.*;
+import arc.scene.ui.*;
+import arc.scene.ui.layout.*;
+import arc.util.*;
+import kotlin.collections.*;
+import mindustry.ai.types.*;
+import mindustry.content.*;
+import mindustry.entities.units.*;
+import mindustry.game.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.input.*;
+import mindustry.ui.*;
+import mindustry.ui.dialogs.*;
+import mindustry.world.*;
+import mindustryX.features.Settings;
+import mindustryX.features.*;
+import mindustryX.features.ui.toolTable.ai.*;
+
+import static mindustry.Vars.*;
+
+public class AuxiliaryTools extends ToolTableBase{
+    private AIController selectAI;
+
+    public AuxiliaryTools(){
+        super("[acid]辅");
+        RStyles.load();
+        rebuild();
+        Events.run(EventType.Trigger.update, () -> {
+            if(selectAI != null && !player.dead()){
+                selectAI.unit(player.unit());
+                selectAI.updateUnit();
+            }
+        });
+    }
+
+    protected void rebuild(){
+        defaults().size(40);
+        if(false) aiButton(new ATRIAI(), Blocks.worldProcessor.region, "ATRI AI");
+        aiButton(new ArcMinerAI(), UnitTypes.mono.region, "矿机AI");
+        aiButton(new ArcBuilderAI(), UnitTypes.poly.region, "重建AI");
+        aiButton(new ArcRepairAI(), UnitTypes.mega.region, "修复AI");
+        aiButton(new DefenderAI(), UnitTypes.oct.region, "保护AI");
+        button(Icon.settingsSmall, Styles.clearNonei, iconMed, this::showAiSettingDialog);
+
+        row();
+        button(new TextureRegionDrawable(Blocks.buildTower.uiIcon), RStyles.clearLineNonei, iconMed, () -> {
+            if(!player.isBuilder()) return;
+            int count = 0;
+            for(Teams.BlockPlan plan : player.team().data().plans){
+                if(player.within(plan.x * tilesize, plan.y * tilesize, buildingRange)){
+                    player.unit().addBuild(new BuildPlan(plan.x, plan.y, plan.rotation, content.block(plan.block), plan.config));
+                    if(++count >= 255) break;
+                }
+            }
+        }).tooltip("在建造列表加入被摧毁建筑");
+        var t = button(new TextureRegionDrawable(Items.copper.uiIcon), RStyles.clearLineNoneTogglei, () -> AutoFill.enable ^= true).tooltip("一键装弹").checked((b) -> AutoFill.enable).get();
+        SettingsV2.bindQuickSettings(t, CollectionsKt.listOf(AutoFill.INSTANCE.getCooldown()));
+        toggleButton(Icon.modeAttack, "autotarget", "自动攻击");
+        toggleButton(new TextureRegionDrawable(UnitTypes.vela.uiIcon), "forceBoost", "强制助推");
+        button(Icon.eyeSmall, RStyles.clearLineNoneTogglei, iconMed, () -> {
+            Settings.toggle("viewMode");
+            boolean setting = Core.settings.getBool("viewMode");
+            UIExt.announce("已" + (setting ? "开启" : "取消") + "视角脱离玩家");
+            if(setting){
+                if(control.input instanceof DesktopInput desktopInput){
+                    desktopInput.panning = true;
+                }
+            }else{
+                Core.camera.position.set(player);
+            }
+        }).tooltip("视角脱离玩家", true).checked(b -> Core.settings.getBool("viewMode"));
+
+        if(!mobile) return;
+        row();
+        toggleButton(Icon.unitsSmall, "指挥模式", () -> control.input.commandMode = !control.input.commandMode).checked(b -> control.input.commandMode);
+        toggleButton(Icon.pause, "暂停建造", () -> control.input.isBuilding = !control.input.isBuilding).checked(b -> control.input.isBuilding);
+        scriptButton(Icon.up, "捡起载荷", () -> control.input.tryPickupPayload());
+        scriptButton(Icon.down, "丢下载荷", () -> control.input.tryDropPayload());
+        scriptButton(new TextureRegionDrawable(Blocks.payloadConveyor.uiIcon), "进入传送带", () -> {
+            Building build = player.buildOn();
+            if(build == null || player.dead()) return;
+            Call.unitBuildingControlSelect(player.unit(), build);
+        });
+    }
+
+    private void aiButton(AIController ai, TextureRegion textureRegion, String describe){
+        table().get().button(new TextureRegionDrawable(textureRegion), RStyles.clearLineNoneTogglei, iconMed, () -> selectAI = selectAI == ai ? null : ai).checked(b -> selectAI == ai).tooltip(describe);
+    }
+
+
+    protected void toggleButton(Drawable icon, String settingName, String description){
+        button(icon, RStyles.clearLineNoneTogglei, iconMed, () -> {
+            Settings.toggle("viewMode");
+            boolean setting = Core.settings.getBool("viewMode");
+            UIExt.announce("已" + (setting ? "开启" : "取消") + description);
+        }).tooltip(description, true).checked(b -> Core.settings.getBool(settingName));
+    }
+
+    protected Cell<ImageButton> toggleButton(Drawable icon, String description, Runnable runnable){
+        return button(icon, RStyles.clearLineNonei, iconMed, runnable).tooltip(description, true);
+    }
+
+    protected void scriptButton(Drawable icon, String description, Runnable runnable){
+        button(icon, Styles.clearNonei, iconMed, runnable).tooltip(description, true);
+    }
+
+    private void showAiSettingDialog(){
+        int cols = (int)Math.max(Core.graphics.getWidth() / Scl.scl(480), 1);
+
+        BaseDialog dialog = new BaseDialog("ARC-AI设定器");
+
+        dialog.cont.table(t -> {
+            t.add("minerAI-矿物筛选器").color(Pal.accent).pad(cols / 2f).center().row();
+            t.image().color(Pal.accent).fillX().row();
+            t.table(c -> {
+                c.add("地表矿").row();
+                c.table(list -> {
+                    int i = 0;
+                    for(Block block : ArcMinerAI.oreAllList){
+                        if(indexer.floorOresCount[block.id] == 0) continue;
+                        if(i++ % 3 == 0) list.row();
+                        list.button(block.emoji() + "\n" + indexer.floorOresCount[block.id], Styles.flatToggleMenut, () -> {
+                            if(ArcMinerAI.oreList.contains(block)) ArcMinerAI.oreList.remove(block);
+                            else if(!ArcMinerAI.oreList.contains(block)) ArcMinerAI.oreList.add(block);
+                        }).tooltip(block.localizedName).checked(k -> ArcMinerAI.oreList.contains(block)).width(100f).height(50f);
+                    }
+                }).row();
+
+                c.add("墙矿").row();
+                c.table(list -> {
+                    int i = 0;
+                    for(Block block : ArcMinerAI.oreAllWallList){
+                        if(indexer.wallOresCount[block.id] == 0) continue;
+                        if(i++ % 3 == 0) list.row();
+                        list.button(block.emoji() + "\n" + indexer.wallOresCount[block.id], Styles.flatToggleMenut, () -> {
+                            if(ArcMinerAI.oreWallList.contains(block)) ArcMinerAI.oreWallList.remove(block);
+                            else if(!ArcMinerAI.oreWallList.contains(block)) ArcMinerAI.oreWallList.add(block);
+                        }).tooltip(block.localizedName).checked(k -> ArcMinerAI.oreWallList.contains(block)).width(100f).height(50f);
+                    }
+                }).row();
+            }).growX();
+        }).growX().row();
+
+        dialog.cont.table(t -> {
+            t.add("builderAI").color(Pal.accent).pad(cols / 2f).center().row();
+            t.image().color(Pal.accent).fillX().row();
+
+            t.table(tt -> {
+                tt.add("重建冷却时间: ");
+                TextField sField = tt.field(ArcBuilderAI.rebuildTime + "", text -> ArcBuilderAI.rebuildTime = Math.max(5f, Float.parseFloat(text))).valid(Strings::canParsePositiveFloat).width(200f).get();
+                tt.slider(5, 200, 5, i -> {
+                    ArcBuilderAI.rebuildTime = i;
+                    sField.setText(ArcBuilderAI.rebuildTime + "");
+                }).width(200f);
+            }).growX();
+        }).growX();
+
+        dialog.addCloseButton();
+        dialog.show();
+    }
+}
