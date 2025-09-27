f89b86b5bfdaa64cce1295872f685f1ab81644fa
way-zer <himc.wicp@gmail.com>
2025-09-27 13:33:30 +0800

    Update HEAD -> v152(8884aec2488f393e2fcd292162b46c37d59250a7)

--- scripts/updateUpstream.sh
+++ scripts/updateUpstream.sh
@@ -4,15 +4,17 @@ function getRef() {
     git ls-tree "$1" "$2" | cut -d' ' -f3 | cut -f1
 }
 
+upstream="$1"
+upstreamRef="$2"
 refHEAD=$(cd .. && getRef HEAD work || exit 1)
 
-git fetch origin "$1" || exit 1
+git fetch "$upstream" "$upstreamRef" || exit 1
 refRemote=$(git rev-parse FETCH_HEAD || exit 1)
 
 if [ "$refHEAD" != "$refRemote" ]; then
   echo "$refHEAD -> $refRemote"
   git reset --hard FETCH_HEAD || (echo "Fail reset" && exit 1)
-  (cd .. && git add --force work && git commit -m "Update HEAD -> $1($refRemote)")
+  (cd .. && git add --force work && git commit -m "Update HEAD -> $upstreamRef($refRemote)")
   echo "Rebuilding patches"
   ../scripts/applyPatches.sh
   echo "After resolve conflict, run '../scripts/genPatches.sh'"
